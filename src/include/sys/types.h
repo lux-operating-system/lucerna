94a0da078916ca3c7ff4b4a5a6c5459d566f85ec
jewelcodes <omarmelghoul01@gmail.com>
2024-09-05 15:08:28 -0400

    include: added more types to sys/types.h

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>         // size_t and ssize_t
 
 // Unix system types
 
@@ -16,7 +17,15 @@ typedef uint64_t nlink_t;
 typedef uint32_t uid_t;
 typedef uint32_t gid_t;
 typedef int32_t pid_t;
-typedef uint64_t off_t;
+typedef uint32_t id_t;
+typedef int64_t off_t;
 typedef uint64_t time_t;
 typedef uint16_t blksize_t;
-typedef uint64_t blkcnt_t;
+typedef int64_t blkcnt_t;
+typedef uint64_t clock_t;
+typedef int16_t clockid_t;
+typedef uint64_t fsblkcnt_t;
+typedef uint64_t fsfilcnt_t;
+typedef int16_t timer_t;
+typedef uint64_t useconds_t;
+typedef int64_t suseconds_t;
