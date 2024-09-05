a69aa95f84fea2733900ad56a6b71909d5c77e76
jewelcodes <omarmelghoul01@gmail.com>
2024-09-05 15:45:20 -0400

    include: fix wctype.h to not redefine constants

--- src/include/wctype.h
+++ src/include/wctype.h
@@ -6,11 +6,8 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
-typedef uint32_t wchar_t;
 typedef uint64_t wint_t;
 typedef int wctype_t;
 typedef int wctrans_t;
-
-#define WCHAR_MIN           0
-#define WCHAR_MAX           99999
