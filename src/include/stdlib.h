2f401ab0f9e5f44fac8d2405229211ae42b8e4db
jewelcodes <omarmelghoul01@gmail.com>
2024-09-10 19:33:31 -0400

    stdlib: added free(), realloc(), and calloc()

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -9,3 +9,5 @@
 
 void *malloc(size_t);
 void free(void *);
+void *calloc(size_t, size_t);
+void *realloc(void *, size_t);
--- src/stdlib/alloc.c
+++ src/stdlib/alloc.c
@@ -7,6 +7,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define BRK_INCREMENTS      65536       // so we don't have to keep making syscalls on every allocation
@@ -19,7 +20,13 @@ struct mallocHeader {
 static void *heapStart = NULL, *heapEnd = NULL;
 static size_t heapSize = 0;
 
+static size_t roundToBrkIncrement(size_t n) {
+    while(n % BRK_INCREMENTS) n++;
+    return n;
+}
+
 void *malloc(size_t n) {
+    if(!n) return NULL;
     struct mallocHeader *hdr;
 
     if(!heapStart || heapStart == (void *)-1) {
@@ -35,10 +42,11 @@ void *malloc(size_t n) {
 
     // possibly allocate more memory
     if(n > heapSize || n > BRK_INCREMENTS) {
-        void *brk = sbrk(n);
+        size_t inc = roundToBrkIncrement(n);
+        void *brk = sbrk(inc);
         if(brk == (void *)-1) return NULL;
 
-        heapSize += n;
+        heapSize += inc;
         heapEnd = heapStart + heapSize;
 
         hdr = brk;
@@ -77,4 +85,41 @@ void *malloc(size_t n) {
     }
 
     return NULL;
+}
+
+void free(void *ptr) {
+    if(!ptr) return;
+
+    struct mallocHeader *h = (struct mallocHeader *)((uintptr_t)ptr - sizeof(struct mallocHeader));
+    h->valid = 0;
+}
+
+void *calloc(size_t num, size_t size) {
+    size_t s = num*size;
+    if(!s) return NULL;
+
+    void *ptr = malloc(s);
+    if(!ptr) return NULL;
+
+    return memset(ptr, 0, s);
+}
+
+void *realloc(void *ptr, size_t newSize) {
+    if(!newSize) return NULL;
+    if(!ptr) return malloc(newSize);
+
+    void *newPtr = malloc(newSize);
+    if(!newPtr) return NULL;
+
+    struct mallocHeader *h = (struct mallocHeader *)((uintptr_t)ptr - sizeof(struct mallocHeader));
+    size_t oldSize = h->next - sizeof(struct mallocHeader);
+
+    if(newSize >= oldSize) {
+        memcpy(newPtr, ptr, oldSize);
+    } else {
+        memcpy(newPtr, ptr, newSize);
+    }
+
+    free(ptr);
+    return newPtr;
 }
