c251d2f235a4e6e770fc6ace6b74cc73958011b3
jewelcodes <omarmelghoul01@gmail.com>
2024-09-11 10:43:10 -0400

    stdlib: integer-string conversions

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -7,7 +7,16 @@
 
 #include <stddef.h>
 
+#define OCTAL       8
+#define DECIMAL     10
+#define HEX         16
+
 void *malloc(size_t);
 void free(void *);
 void *calloc(size_t, size_t);
 void *realloc(void *, size_t);
+
+char *itoa(int, char *, int);
+int atoi(const char *);
+char *ltoa(long, char *, int);
+long atol(const char *);
---
+++ src/stdlib/intstring.c
@@ -0,0 +1,82 @@
+/*
+ * lucerna - an implementation of the standard C library
+ * Omar Elghoul, 2024
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+
+char *itoa(int n, char *buffer, int radix) {
+    return ltoa((long)n, buffer, radix);
+}
+
+int atoi(const char *s) {
+    return (long)atol(s);
+}
+
+char *ltoa(long n, char *buffer, int radix) {
+    if(!radix || radix > HEX) return NULL;
+
+    if(!n) {
+        buffer[0] = '0';
+        buffer[1] = 0;
+        return buffer;
+    }
+
+    int length = 0;
+
+    uint64_t un = (uint64_t)n;
+
+    while(un) {
+        // convert digit by digit and then reverse the string
+        uint64_t digit = un % radix;
+
+        if(digit >= 10) {
+            buffer[length] = 'a' + digit - 10;
+        } else {
+            buffer[length] = '0' + digit;
+        }
+
+        length++;
+        un /= radix;
+    }
+
+    buffer[length] = 0;   // null terminator
+
+    // now reverse the string
+    if(length >= 2) {
+        for(int i = 0; i < length/2; i++) {
+            char tmp = buffer[i];
+            buffer[i] = buffer[length-i-1];
+            buffer[length-i-1] = tmp;
+        }
+    }
+
+    return buffer;
+}
+
+long atol(const char *s) {
+    long v = 0;
+    int len = 0;
+
+    while(s[len] >= '0' && s[len] <= '9') {
+        len++;      // didn't use strlen so we can only account for numerical characters
+    }
+
+    if(!len) return 0;
+
+    char buffer[20];
+
+    long multiplier = 1;
+    for(int i = 1; i < len; i++) {
+        multiplier *= 10;
+    }
+
+    for(int i = 0; i < len; i++) {
+        long digit = s[i] - '0';
+        v += (digit * multiplier);
+        multiplier /= 10;
+    }
+
+    return v;
+}
