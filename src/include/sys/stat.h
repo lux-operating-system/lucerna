8c65cbb60847af29e2058d4bc6478bef80ef0333
jewelcodes <omarmelghoul01@gmail.com>
2024-09-15 18:31:00 -0400

    stat: added sys/stat.h

---
+++ src/include/sys/stat.h
@@ -0,0 +1,24 @@
+/*
+ * lucerna - an implementation of the standard C library
+ * Omar Elghoul, 2024
+ */
+
+#pragma once
+
+#include <sys/types.h>
+
+struct stat {
+    dev_t st_dev;
+    ino_t st_ino;
+    mode_t st_mode;
+    nlink_t st_nlink;
+    uid_t st_uid;
+    gid_t st_gid;
+    dev_t st_rdev;
+    off_t st_size;
+    time_t st_atime, st_mtime, st_ctime;
+    blksize_t st_blksize;
+    blkcnt_t st_blocks;
+};
+
+int stat(const char *, struct stat *);
