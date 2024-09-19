13729b993e779d23a0936cd4476abe801d25435a
jewelcodes <omarmelghoul01@gmail.com>
2024-09-18 21:58:45 -0400

    io: prototypes for out/in instructions

--- src/include/sys/io.h
+++ src/include/sys/io.h
@@ -8,3 +8,9 @@
 #include <stdint.h>
 
 int ioperm(uintptr_t, uintptr_t, int);
+void outb(uintptr_t, uint8_t);
+void outw(uintptr_t, uint16_t);
+void outd(uintptr_t, uint32_t);
+uint8_t inb(uintptr_t);
+uint16_t inw(uintptr_t);
+uint32_t ind(uintptr_t);
