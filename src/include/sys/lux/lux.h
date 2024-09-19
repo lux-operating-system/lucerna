3487f5276a73a2cb8c729bf6b2bfc02e82fe1082
jewelcodes <omarmelghoul01@gmail.com>
2024-09-19 18:34:44 -0400

    lux: added irq handler creator

--- src/include/sys/lux/lux.h
+++ src/include/sys/lux/lux.h
@@ -7,4 +7,15 @@
 
 #pragma once
 
+#include <stdint.h>
+
+typedef struct {
+    char name[256];     // device name
+    char driver[256];   // name of server/driver prefixed with "lux:///ks"
+    int kernel;         // 1 for kernel-level IRQ handler, 0 for user space
+    uintptr_t khandler; // entry point for kernel IRQ handlers
+    int level, high;    // trigger mode
+} IRQHandler;
+
 int execrdv(const char *, const char **);
+int irq(int, IRQHandler *);
