7d9956b8dcdb616ca1950e9c7560719c31209c0b
jewelcodes <omarmelghoul01@gmail.com>
2024-09-20 19:46:23 -0400

    stdlib: implemented a proper heap allocator in malloc()

--- src/stdlib/alloc.c
+++ src/stdlib/alloc.c
@@ -12,6 +12,9 @@
 
 #define BRK_INCREMENTS      65536       // so we don't have to keep making syscalls on every allocation
 
+static void *programBreak = NULL;
+static size_t dataSegmentSize = 0;
+
 struct mallocHeader {
     size_t next;           // bytes to skip to next block
     int valid;
@@ -24,17 +27,66 @@ static size_t roundToBrkIncrement(size_t n) {
 
 void *malloc(size_t n) {
     if(!n) return NULL;
+    void *ptr;
     struct mallocHeader *hdr;
+    size_t increment = roundToBrkIncrement(n + sizeof(struct mallocHeader));
 
-    size_t nextBrk = roundToBrkIncrement(n + sizeof(struct mallocHeader) + 4096);
-    void *brk = sbrk(nextBrk);
-    if(!brk || brk == (void *)-1) return NULL;
+    if(!programBreak || !dataSegmentSize) {
+        // for the initial allocation
+        ptr = sbrk(increment);
+        if(ptr == (void *) -1) return NULL;
 
-    hdr = brk;
-    hdr->valid = 1;
-    hdr->next = n;
+        programBreak = ptr;
+        dataSegmentSize += increment;
 
-    return (void *)((uintptr_t)brk + sizeof(struct mallocHeader));
+        hdr = (struct mallocHeader *) ptr;
+        hdr->next = n + sizeof(struct mallocHeader);
+        hdr->valid = 1;
+
+        ptr = (void *)((uintptr_t)ptr + sizeof(struct mallocHeader));
+        return ptr;
+    }
+
+search:
+    // for all other allocations, search for an appropriate free block
+    size_t position = 0;
+    hdr = (struct mallocHeader *) programBreak;
+    while(position < (dataSegmentSize - sizeof(struct mallocHeader))) {
+        if(hdr->valid) {
+            // used block, skip
+            position += hdr->next;
+            hdr = (struct mallocHeader *)((uintptr_t)hdr + hdr->next);
+        } else {
+            // free block, check size
+            size_t size;
+            if(!hdr->next) size = dataSegmentSize - position - sizeof(struct mallocHeader);
+            else size = hdr->next - sizeof(struct mallocHeader);
+
+            if(size >= n) {
+                // found a free block
+                if(!hdr->next) hdr->next = n + sizeof(struct mallocHeader);
+
+                hdr->valid = 1;
+                ptr = (void *)((uintptr_t)hdr + sizeof(struct mallocHeader));
+                return ptr;
+            } else {
+                // free block but not large enough, skip
+                position += size + sizeof(struct mallocHeader);
+                hdr = (struct mallocHeader *)((uintptr_t)hdr + hdr->next);
+            }
+        }
+    }
+
+    // if we make it here then we didn't find a free block, to try to allocate more memory
+    ptr = sbrk(increment);
+    if(ptr == (void *)-1) return NULL;  // system is out of memory
+
+    hdr = (struct mallocHeader *) ptr;
+    hdr->next = 0;
+    hdr->valid = 0;
+
+    dataSegmentSize += increment;
+    goto search;
 }
 
 void free(void *ptr) {
