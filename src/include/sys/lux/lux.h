ddcc824d55b33e7b2545b3bfe3ba714f2fcca2ff
jewelcodes <omarmelghoul01@gmail.com>
2024-09-26 18:18:51 -0400

    lux: added mmio() flags

--- src/include/sys/lux/lux.h
+++ src/include/sys/lux/lux.h
@@ -9,6 +9,12 @@
 
 #include <sys/types.h>
 
+#define MMIO_R          0x01    // read perms
+#define MMIO_W          0x02    // write perms
+#define MMIO_X          0x04    // execute perms
+#define MMIO_CD         0x08    // cache disable
+#define MMIO_ENABLE     0x80    // create mapping; clear to unmap
+
 typedef struct {
     char name[256];     // device name
     char driver[256];   // name of server/driver prefixed with "lux:///ks"
