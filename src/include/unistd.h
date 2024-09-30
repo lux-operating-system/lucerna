81f720ea263311b681c703b1e73d1c6681b128e2
jewelcodes <omarmelghoul01@gmail.com>
2024-09-30 16:30:37 -0400

    unistd: added env and exec() declarations

--- src/include/unistd.h
+++ src/include/unistd.h
@@ -14,11 +14,20 @@
 #define SEEK_CUR                2
 #define SEEK_END                3
 
+extern char **environ;
 void exit(int);
 void _exit(int);
 pid_t fork(void);
 pid_t vfork(void);
 int sched_yield(void);
+
+int execl(const char *, const char *, ...);
+int execv(const char *, const char **);
+int execle(const char *, const char *, ...);
+int execve(const char *, const char **, const char **);
+int execlp(const char *, const char *, ...);
+int execvp(const char *, const char **);
+
 pid_t getpid(void);
 pid_t gettid(void);
 uid_t getuid(void);
@@ -28,3 +37,4 @@ ssize_t read(int, void *, size_t);
 ssize_t write(int, const void *, size_t);
 void *sbrk(intptr_t);
 off_t lseek(int, off_t, int);
+
