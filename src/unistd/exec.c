fcce551d0cb511f15f848b47ffc01d7ba1779e4d
jewelcodes <omarmelghoul01@gmail.com>
2024-09-30 21:02:08 -0400

    unistd: declared environ

---
+++ src/unistd/exec.c
@@ -0,0 +1,10 @@
+/*
+ * lucerna - an implementation of the standard C library
+ * Omar Elghoul, 2024
+ */
+
+/* Implementation of unistd.h: Program execution and environmental variables */
+
+#include <unistd.h>
+
+char **environ;
