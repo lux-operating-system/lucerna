d9b2223638cd70aed2da82407349771409b7e6e8
jewelcodes <omarmelghoul01@gmail.com>
2024-10-08 00:08:06 -0400

    float: floating point declarations

---
+++ src/include/float.h
@@ -0,0 +1,33 @@
+/*
+ * lucerna - an implementation of the standard C library
+ * Omar Elghoul, 2024
+ */
+
+#pragma once
+
+#define FLT_ROUNDS          1
+#define FLT_EVAL_METHOD     2
+#define FLT_RADIX           2
+
+#define FLT_MANT_DIG        10
+#define DBL_MANT_DIG        20
+#define LDBL_MANT_DIG       20
+#define DECIMAL_DIG         20
+#define FLT_DIG             FLT_MANT_DIG
+#define DBL_DIG             DBL_MANT_DIG
+#define LDBL_DIG            LDBL_MANT_DIG
+#define FLT_MIN_10_EXP      -37
+#define DBL_MIN_10_EXP      -48
+#define LDBL_MIN_10_EXP     -48
+#define FLT_MAX_10_EXP      37
+#define DBL_MAX_10_EXP      48
+#define LDBL_MAX_10_EXP     48
+#define FLT_MAX             1E+37
+#define DBL_MAX             1E+48
+#define LDBL_MAX            1E+48
+#define FLT_MIN             1E-37
+#define DBL_MIN             1E-37
+#define LDBL_MIN            1E-37
+#define FLT_EPSILON         1E-8
+#define DBL_EPSILON         1E-12
+#define LDBL_EPSILON        1E-12
