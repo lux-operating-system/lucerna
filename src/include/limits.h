dcbcc506a236c64294819c2c2efe749583199053
jewelcodes <omarmelghoul01@gmail.com>
2024-11-30 15:20:13 -0500

    limits: definitions for char and int limits

--- src/include/limits.h
+++ src/include/limits.h
@@ -8,3 +8,9 @@
 #define PATH_MAX        2048
 #define NAME_MAX        256
 #define TTY_NAME_MAX    128
+
+#define CHAR_BIT        8
+#define CHAR_MAX        255
+#define CHAR_MIN        0
+
+#define INT_MAX         2147483647
