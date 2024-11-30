36c3495453254c6d7f52866765cfd412ff5c7972
jewelcodes <omarmelghoul01@gmail.com>
2024-11-30 16:33:30 -0500

    math: implemented log()

--- src/math.c
+++ src/math.c
@@ -25,3 +25,30 @@ double exp(double x) {
 float expf(float x) {
     return (float) expl((long double) x);
 }
+
+long double logl(long double x) {
+    if(x <= 0.0) {
+        errno = EDOM;
+        return NAN;
+    }
+
+    long double result = 0.0;
+    long double term = (x-1.0) / (x+1.0);
+    long double z = term;
+    long double z2 = z*z;
+
+    for(int i = 1; i < 100; i += 2) {
+        result += (1.0 / i) * z;
+        z *= z2;
+    }
+
+    return result;
+}
+
+float logf(float x) {
+    return (float) logl((long double) x);
+}
+
+double log(double x) {
+    return (double) logl((long double) x);
+}
