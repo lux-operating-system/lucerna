79b5f4c5305194be00eb4360ce1f88c809048d0f
jewelcodes <omarmelghoul01@gmail.com>
2024-11-30 17:56:44 -0500

    limits: declared limits for long, long long, and short

--- src/include/limits.h
+++ src/include/limits.h
@@ -13,5 +13,26 @@
 #define CHAR_MAX        255
 #define CHAR_MIN        0
 
-#define INT_MIN         0
+#define WORD_BIT        32
+#define INT_MIN         -2147483647
 #define INT_MAX         2147483647
+#define UINT_MAX        4294967295
+
+#define LONG_BIT        64
+#define LONG_MIN        -9223372036854775807
+#define LONG_MAX        9223372036854775807
+
+#define LLONG_MIN       -9223372036854775807
+#define LLONG_MAX       9223372036854775807
+#define ULONG_MAX       18446744073709551615
+#define ULLONG_MAX      18446744073709551615
+
+#define SCHAR_MAX       127
+#define UCHAR_MAX       255
+#define SCHAR_MIN       -128
+
+#define MB_LEN_MAX      1
+
+#define USHRT_MAX       65535
+#define SHRT_MIN        -32767
+#define SHRT_MAX        32767
