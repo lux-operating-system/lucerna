8f5321af5ce1dd75c43e2e38cfe3ee482a8a00ff
jewelcodes <omarmelghoul01@gmail.com>
2024-12-29 23:30:32 -0500

    fcntl: declared O_CLOFORK

--- src/include/fcntl.h
+++ src/include/fcntl.h
@@ -28,6 +28,7 @@ struct flock {
 #define O_RSYNC                 0x0200
 #define O_SYNC                  0x0400
 #define O_TRUNC                 0x0800
+#define O_CLOFORK               0x1000
 #define O_ACCMODE               (O_RDONLY | O_WRONLY | O_RDWR)
 
 /* fcntl() commands */
