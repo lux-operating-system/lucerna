088fdd9cb9bdea369f26da75703b1cb4c102d2db
jewelcodes <omarmelghoul01@gmail.com>
2025-01-10 23:13:39 -0500

    mount: prototype and macros for umount() and umount2()

--- src/include/sys/mount.h
+++ src/include/sys/mount.h
@@ -5,4 +5,11 @@
 
 #pragma once
 
+#define MNT_FORCE               0x01
+#define MNT_DETACH              0x02
+#define MNT_EXPIRE              0x04
+#define UMOUNT_NOFOLLOW         0x08
+
 int mount(const char *, const char *, const char *, int, void *);
+int umount(const char *);
+int umount2(const char *, int);
