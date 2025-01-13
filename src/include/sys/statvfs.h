ada7d2261e32b049c0bd1b6d3e5f4b2cd8be3acb
jewelcodes <omarmelghoul01@gmail.com>
2025-01-13 01:05:15 -0500

    statvfs: structure and prototypes for fstatvfs() and statvfs()

---
+++ src/include/sys/statvfs.h
@@ -0,0 +1,28 @@
+/*
+ * lucerna - an implementation of the standard C library
+ * Omar Elghoul, 2024
+ */
+
+#pragma once
+
+#include <sys/types.h>
+
+#define ST_RDONLY           0x01
+#define ST_NOSUID           0x02
+
+struct statvfs {
+    unsigned long f_bsize;
+    unsigned long f_frsize;
+    fsblkcnt_t f_blocks;
+    fsblkcnt_t f_bfree;
+    fsblkcnt_t f_bavail;
+    fsfilcnt_t f_files;
+    fsfilcnt_t f_ffree;
+    fsfilcnt_t f_favail;
+    unsigned long f_sid;
+    unsigned long f_flag;
+    unsigned long f_namemax;
+};
+
+int fstatvfs(int, struct statvfs *);
+int statvfs(const char *, struct statvfs *);
