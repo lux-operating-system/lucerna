c8775c9019b0d1f35ac8f06f2e1fa07cfbac13ea
jewelcodes <omarmelghoul01@gmail.com>
2025-01-22 12:46:24 -0500

    stdlib: follow linux implementation of abort()

--- src/stdlib/abort.c
+++ src/stdlib/abort.c
@@ -4,7 +4,20 @@
  */
 
 #include <signal.h>
+#include <stddef.h>
+
+/* This implementation follows the Linux implementation because POSIX is a
+ * little vague about the exact step-by-step procedure of abort()
+ * https://man7.org/linux/man-pages/man3/abort.3.html
+ */
 
 void abort(void) {
+    sigset_t sig;
+    sigemptyset(&sig);
+    sigaddset(&sig, SIGABRT);
+    sigprocmask(SIG_UNBLOCK, &sig, NULL);
+    raise(SIGABRT);
+
+    signal(SIGABRT, SIG_DFL);
     raise(SIGABRT);
 }
